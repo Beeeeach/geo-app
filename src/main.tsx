1e91d34df67c0e8e51dde4f19aa92673c0c9501e
Beeeeach <beach_naoki@outlook.jp>
2026-08-03 18:35:52 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
+<<<<<<< HEAD
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+=======
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+>>>>>>> 30dca02bd58e331c72bf234f5087adfda38e9ffb
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+<<<<<<< HEAD
+  </StrictMode>
+);
+=======
   </StrictMode>,
 )
+>>>>>>> 30dca02bd58e331c72bf234f5087adfda38e9ffb
